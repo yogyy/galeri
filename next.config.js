7d332ce5a65b6c97607b7534819e165ff5068765
yogyy <yogimr261@gmail.com>
2024-04-24 15:04:04 +0700

    chore: next config for auth
    add bundler analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt")
+    return config
+  },
+}
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
