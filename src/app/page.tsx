f6c3b54860edfce0b3b2c10d5f5556d432ca7181
yogyy <yogimr261@gmail.com>
2024-04-25 11:00:18 +0700

    chore: move to root instead /gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { redirect } from "next/navigation"
-
-import { validateRequest } from "@/lib/auth"
+import { getPhotos } from "@/lib/get-photos"
+import { Card } from "@/components/ui/card"
+import { Collage } from "@/components/collage"
 import { Title } from "@/components/title"
-import { ProfileForm } from "@/app/(public)/gallery/_comps/form-zod"
 
 export default async function Home() {
-  const { user } = await validateRequest()
-  if (!user) {
-    redirect("/gallery")
-  }
-
+  const photos = await getPhotos()
   return (
-    <main className="container relative max-w-xl min-h-[calc(100dvh_-_48px)] space-y-5">
-      <Title
-        title="Atmin Page"
-        subtitle={
-          <h2 className="text-cyan">{`What's good ${user.username}`}</h2>
-        }
-        className="py-20"
-      />
-      <h3 className="text-2xl">Add new Photo</h3>
-      <ProfileForm />
+    <main className="container pt-24">
+      <Card className="flex flex-col p-0">
+        <div className="bg-bg-darker py-6">
+          <div className="container flex max-w-screen-md flex-col gap-6">
+            <Title
+              title="irasuto"
+              titleLabel={
+                <span className="text-base text-text-tertiary">イラスト</span>
+              }
+              subtitle="This is where I store my collection of some of the most gorgeous illustrations related to Japanese popular culture that I’ve found on Twitter. Enjoy :)"
+            />
+          </div>
+        </div>
+        <Collage photos={photos} />
+      </Card>
     </main>
   )
 }
