6024cc961a9a2345bdefe012b809e4c78b67d4a1
yogyy <yogimr261@gmail.com>
2024-04-25 19:01:17 +0700

    chore: add config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt")
     return config
   },
+
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 }
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
