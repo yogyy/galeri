fa29d9ce264c1b28d51d380672818553b37dd45a
yogyy <yogimr261@gmail.com>
2024-04-28 00:12:34 +0700

    chore: move to one file

--- src/components/lazy-image.tsx
+++ src/components/lazy-image.tsx
@@ -2,11 +2,12 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import Image, { ImageProps } from "next/image"
 import { useInView } from "react-intersection-observer"
 
 import { cn } from "@/lib/utils"
 
-export function LazyImage({
+export function LazyImageBlob({
   src,
   alt,
   className,
@@ -40,7 +41,7 @@ export function LazyImage({
   }, [inView, src, dataUrl])
 
   return (
-    <div ref={ref} className="bg-daw-main-100">
+    <div ref={ref}>
       <img
         src={
           dataUrl ||
@@ -53,6 +54,37 @@ export function LazyImage({
           dataUrl ? "opacity-100" : "opacity-0",
           className
         )}
+      />
+    </div>
+  )
+}
+
+export function LazyImage({ src, alt, className, ...props }: ImageProps) {
+  const { ref, inView } = useInView({ rootMargin: "-10% 0px" })
+  const [dataUrl, setDataUrl] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!inView || !src || dataUrl) return
+    const timeout = setTimeout(() => setDataUrl(src as string), 100)
+
+    return () => clearTimeout(timeout)
+  }, [inView, src, dataUrl])
+
+  return (
+    <div ref={ref}>
+      <Image
+        src={
+          dataUrl ||
+          `data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${props.width} ${props.height}'%3e%3c/svg%3e`
+        }
+        alt={alt}
+        className={cn(
+          "transition duration-600",
+          dataUrl ? "opacity-100" : "opacity-0",
+          className
+        )}
+        unoptimized
+        {...props}
       />
     </div>
   )
