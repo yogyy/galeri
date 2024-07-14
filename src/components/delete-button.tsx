ed71c699e55bdf2449c3d949b21d698a0d10a3fe
yogyy <yogimr261@gmail.com>
2024-07-15 01:20:28 +0700

    chore: add confirmation step to delete

--- src/components/delete-button.tsx
+++ src/components/delete-button.tsx
@@ -2,16 +2,37 @@ import { Button } from "@/components/ui/button"
 import { Trash } from "@/components/icons"
 import { deletePhoto } from "@/app/_lib/actions"
 
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from "./ui/dialog"
+
 export function DeleteButton({ id }: { id: number }) {
   const action = deletePhoto.bind(null, id)
   return (
-    <form
-      action={deletePhoto.bind(null, id)}
-      className="absolute right-3 top-3 transition sm:-translate-y-1 sm:opacity-0 sm:group-hover:translate-y-0 sm:group-hover:opacity-100"
-    >
-      <Button formAction={action} size="sm" type="submit">
-        <Trash /> Remove
-      </Button>
-    </form>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="absolute right-3 top-3 transition sm:-translate-y-1 sm:opacity-0 sm:group-hover:translate-y-0 sm:group-hover:opacity-100">
+          <Button size="sm">
+            <Trash /> Remove
+          </Button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-sm bg-black/70">
+        Confirm Delete This Photo
+        <div className="flex justify-between">
+          <form>
+            <Button
+              formAction={action}
+              size="sm"
+              type="submit"
+              className="text-mint"
+            >
+              Remove
+            </Button>
+          </form>
+          <DialogClose asChild className="font-bold text-red">
+            <Button size="sm">cancel</Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
