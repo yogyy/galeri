7cbcb9f4c3db2f0b9fd79692500ff5618fb2891e
yogyy <yogimr261@gmail.com>
2024-07-18 01:21:46 +0700

    chore: using react-hook-form
    emang bener handle form pake react-hook-form

--- src/components/delete-button.tsx
+++ src/components/delete-button.tsx
@@ -1,16 +1,63 @@
+"use client"
+
+import { useTransition } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { z } from "zod"
+
+import { getErrorMessage } from "@/lib/handle-error"
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
 import { Trash } from "@/components/icons"
 import { deletePhoto } from "@/app/(admin)/manage/_lib/actions"
 
-import { Dialog, DialogClose, DialogContent, DialogTrigger } from "./ui/dialog"
-
 export function DeleteButton({ id }: { id: number }) {
-  const action = deletePhoto.bind(null, id)
+  const [isPending, startTransition] = useTransition()
+
+  const deleteSchema = z.object({
+    id: z.number().min(1),
+  })
+
+  const form = useForm<z.infer<typeof deleteSchema>>({
+    resolver: zodResolver(deleteSchema),
+    defaultValues: {
+      id,
+    },
+  })
+
+  function onSubmit({ id }: z.infer<typeof deleteSchema>) {
+    startTransition(() => {
+      toast.promise(deletePhoto(id), {
+        loading: "deleting photo",
+        success() {
+          form.reset()
+          return "photo deleted"
+        },
+        error(error) {
+          return getErrorMessage(error)
+        },
+      })
+    })
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="absolute right-3 top-3 transition sm:-translate-y-1 sm:opacity-0 sm:group-hover:translate-y-0 sm:group-hover:opacity-100">
-          <Button size="sm">
+          <Button size="sm" className="bg-[#ff453a80]">
             <Trash /> Remove
           </Button>
         </div>
@@ -18,17 +65,32 @@ export function DeleteButton({ id }: { id: number }) {
       <DialogContent className="max-w-sm bg-black/70">
         Confirm Delete This Photo
         <div className="flex justify-between">
-          <form>
-            <Button
-              formAction={action}
-              size="sm"
-              type="submit"
-              variant="ghost"
-              className="text-mint"
-            >
-              Remove
-            </Button>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="id"
+                render={({ field }) => (
+                  <FormItem className="sr-only">
+                    <FormControl>
+                      <input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={isPending}
+                variant="ghost"
+                size="sm"
+                className="text-mint"
+              >
+                Ye..
+              </Button>
+            </form>
+          </Form>
+
           <DialogClose asChild className="font-bold text-red">
             <Button size="sm" variant="ghost">
               cancel
