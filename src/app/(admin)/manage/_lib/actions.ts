ee970f78070f194bf4424a00f14408d29ddda50a
yogyy <yogimr261@gmail.com>
2024-07-22 08:04:48 +0700

    feat: add nanoid package to generateRandomId

--- src/app/(admin)/manage/_lib/actions.ts
+++ src/app/(admin)/manage/_lib/actions.ts
@@ -5,14 +5,18 @@ import { db } from "@/db"
 import { photo } from "@/db/schema"
 import { eq } from "drizzle-orm"
 
+import { generateRandId } from "@/lib/utils"
+
 import { UploadPhotoSchema } from "./validation"
 
 export const uploadNewPhoto = async (input: UploadPhotoSchema) => {
+  const photoId = generateRandId("i")
   try {
     await db
       .insert(photo)
       .values({
         ...input,
+        id: photoId,
         width: parseInt(input.width),
         height: parseInt(input.height),
       })
@@ -24,7 +28,7 @@ export const uploadNewPhoto = async (input: UploadPhotoSchema) => {
 }
 
 export const deletePhoto = async (id: string) => {
-  await db.delete(photo).where(eq(photo.id, id)).returning()
+  await db.delete(photo).where(eq(photo.id, id))
 
   revalidateTag("photo")
 }
